f16be60587506f1aaaab558bc3c7cd5ccd378988
JulianGonzalez <JulianAgustinGonzalez01@hotmail.com>
2021-11-25 00:04:25 -0300

    Agrego JavaScript

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 const input = document.querySelector('input');
 const addBtn = document.querySelector('.btn-add');
 const ul = document.querySelector('ul');
-const empty = document.querySelector('.empty')
+const empty = document.querySelector('.empty');
 
 addBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const text = input.value
+    const text = input.value;
 
-    const li = document.createElement('li');
-    const p = document.createElement('p');
-    p.textContent = text;
+    if(text !== ""){
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.textContent = text;
 
-    li.appendChild(p);
-    ul.appendChild(li);
-})
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
+
+        input.value = "";
+        empty.style.display = "none";
+    }
+    
+});
+
+function addDeleteBtn() {
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "btn-delete";
+
+    deleteBtn.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+
+        const items = document.querySelectorAll('li');
+
+        if(items.lenght === 0){
+            empty.style.display = "block";
+        }
+    });
+    return deleteBtn;
+}
